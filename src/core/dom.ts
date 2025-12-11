c05b1ff4ac4d46ea83760a72eb9a3d1cd191e881
= <hannes.vinzens@fau.de>
2025-12-11 10:51:28 +0100

    initial commit

---
+++ src/core/dom.ts
@@ -0,0 +1,40 @@
+export interface Node {
+	// common to all nodes
+	children: Node[];
+
+	// specific to each node
+	node_type: NodeType;
+}
+
+type domText = string;
+
+type domElement = {
+	tag_name: string;
+	attrs: AttrMap;
+};
+
+type NodeType = domText | domElement;
+
+type AttrMap = Map<string, string>;
+
+// Functions for creating Nodes
+export const textNode = (txt: string): Node => {
+	return {
+		children: [],
+		node_type: txt,
+	};
+};
+
+export const elemNode = (
+	tagName: string,
+	attrs: AttrMap,
+	children: Node[],
+): Node => {
+	return {
+		children: children,
+		node_type: {
+			tag_name: tagName,
+			attrs: attrs,
+		},
+	};
+};
