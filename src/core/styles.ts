22c0793b9f09df22d7e37b7faf01822a850025a1
= <hannes.vinzens@fau.de>
2025-12-13 15:30:56 +0100

    added stlye types

---
+++ src/core/styles.ts
@@ -0,0 +1,29 @@
+interface Stylesheet {
+	rules: Rule[];
+}
+
+interface Rule {
+	selectors: Selector[];
+	declarations: Declaration[];
+}
+
+type Selector = TagName | ID | Class;
+
+type TagName = string;
+type ID = string;
+type Class = string;
+
+type Declaration = {
+    name: string,
+    value: Value
+};
+
+type Value = Keyword | Size | ColorValue
+type Keyword = string
+type Size = string
+type ColorValue {
+    r: number,
+    g: number,
+    b: number,
+    a: number
+}
